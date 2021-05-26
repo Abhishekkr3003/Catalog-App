0e154628d7a6f9ea1205f988641c2c574787afc6
Abhishekkr3003 <abhishekkr3003@gmail.com>
2021-05-27 00:55:04 +0530

    Complete day2

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   var toBePrinted = "Android App!";
-  int p1=1;
-  int p2=2;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog"),
+      ),
+      drawer: Drawer(),
       body: Center(
         child: Container(
           child: Text("This is $toBePrinted"),
